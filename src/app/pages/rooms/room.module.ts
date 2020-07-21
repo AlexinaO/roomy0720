94af2c4e1c28d38019cd82c9c62ce64474e9621a
Vincent <vleclerc@inow.fr>
2020-07-21 10:28:53 +0200

    add other modules and components

--- src/app/pages/rooms/room.module.ts
+++ src/app/pages/rooms/room.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { MaterialModule } from 'src/app/material.module';
+import { ListRoomComponent } from './list-room/list-room.component';
+import { AddRoomComponent } from './add-room/add-room.component';
 
 @NgModule({
     declarations: [
-        FooterComponent
+        FooterComponent,
+        ListRoomComponent,
+        AddRoomComponent
     ],
     imports: [
         CommonModule,
